92d414716bcd2f6c30d45fa0b6581f17784a28b9
katamartin <katasm@gmail.com>
2022-11-02 10:57:32 -0700

    Use remark-mdx-code-meta and ES modules in Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
-const path = require('path')
+import path from 'path'
+import remarkMdxCodeMeta from 'remark-mdx-code-meta'
+import nextMDX from '@next/mdx'
+import * as url from 'url'
 
 const isDev =
   process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
 
-const withMDX = require('@next/mdx')({
+const withMDX = nextMDX({
   extension: /\.mdx?$/,
+  options: { remarkPlugins: [remarkMdxCodeMeta], format: 'mdx' },
 })
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
-module.exports = withMDX({
+export default withMDX({
   pageExtensions: ['js', 'jsx', 'mdx', 'md'],
   webpack: (config, options) => {
     if (options.isServer) {
