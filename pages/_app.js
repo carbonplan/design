a9d59976f260f837d13957653add54dea536e1ef
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-06 09:09:43 -0600

    add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import PlausibleProvider from 'next-plausible'
 import { ThemeProvider } from 'theme-ui'
 import theme from '@carbonplan/theme'
 import '@carbonplan/components/globals.css'
@@ -5,8 +7,10 @@ import '@carbonplan/components/fonts.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider domain='carbonplan.org'>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   )
 }
